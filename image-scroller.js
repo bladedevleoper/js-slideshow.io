d9b0be503ea3b7430d68a2756551aa82a1d3f832
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2019-03-05 16:53:15 +0000

    Update image-scroller.js

--- image-scroller.js
+++ image-scroller.js
@@ -1,5 +1,24 @@
 const imgDisplay = document.querySelector('#image-display');
+// const rightArrow = document.querySelector('#right');
 
-imgDisplay.addEventListener('click', () => {
-    console.log('picture being clicked');
+const allImages = document.querySelector('#img-container');
+
+//gets all images
+let all = allImages.querySelectorAll('img');
+
+//get current array number
+//then store that in a variable
+//then swap the image in the image-display with the array pointer
+
+
+imgDisplay.addEventListener('click', (e) => {
+
+    if (e.target.tagName == 'SPAN' && e.target.id == 'right') {
+        console.log('right arrow being pressed');
+    }
+
+    if (e.target.tagName == 'SPAN' && e.target.id == 'left') {
+        console.log('left arrow being pressed');
+    }
+
 });
