73353edc6e9a9674ac42ad3bb7ff5ac20e3dff4b
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2019-03-05 16:59:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+<?php $title = 'Simple JS SlideShow"; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title><?= $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
