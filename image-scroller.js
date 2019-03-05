71b789612870b5af81ef9ee0d4316ba2abb7062b
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2019-03-05 16:08:17 +0000

    scroll-update

---
+++ image-scroller.js
@@ -0,0 +1,5 @@
+const imgDisplay = document.querySelector('#image-display');
+
+imgDisplay.addEventListener('click', () => {
+    console.log('picture being clicked');
+});
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class="container">
+
+        <div id="image-display">
+        <img id="img-size" src="../javascript_practice/img/image1.jpg"/>
+            <span id="left">&#10096;</span>
+            <span id="right">&#10097;</span>
+        </div>
+
+        <div id="img-container">
+            <img id="img-size" src="../javascript_practice/img/image1.jpg" hidden/>
+        </div>
+    
+    </div>
+    <!-- <script src="../javascript_practice/js/app.js"></script> -->
+    <script src="../javascript_practice/js/image-scroller.js"></script>
+</body>
+</html>
