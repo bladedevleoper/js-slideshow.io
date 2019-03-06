fba178c4928cc584b857cc7a073d868606a930d6
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2019-03-06 16:31:01 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php $title = 'Simple JS SlideShow'; ?>
+<?php 
+
+    $title = 'Simple JS SlideShow';
+   
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="../image-slider/css/stylesheet.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
 </head>
@@ -15,20 +19,20 @@
     <div class="container">
 
         <div id="image-display" class="mx-auto">
-        <img id="img-size" class="selector" src="../js-slideshow.io/img/image1.jpg"/>
+        <!-- <img id="img-size" class="selector" /> -->
             <span id="left">&#10096;</span>
             <span id="right">&#10097;</span>
         </div>
 
         <div id="img-container">
-            <img id="img-size" src="../js-slideshow.io/img/image1.jpg" hidden/>
-            <img id="img-size" src="../js-slideshow.io/img/image2.jpg" hidden/>
-            <img id="img-size" src="../js-slideshow.io/img/image3.jpg" hidden/>
-            <img id="img-size" src="../js-slideshow.io/img/image4.jpg" hidden/>
+            <img id="img-size" src="../image-slider/img/image1.jpg" hidden/>
+            <img id="img-size" src="../image-slider/img/image2.jpg" hidden/>
+            <img id="img-size" src="../image-slider/img/image3.jpg" hidden/>
+            <img id="img-size" src="../image-slider/img/image4.jpg" hidden/>
         </div>
     
     </div>
     <!-- <script src="../javascript_practice/js/app.js"></script> -->
-    <script src="../js-slideshow.io/js/image-scroller.js"></script>
+    <script src="../image-slider/js/image-scroller.js"></script>
 </body>
 </html>
